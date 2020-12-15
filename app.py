c909e006c26e3a837bc10513d82cf4b85f1abedd
Inferus-arch <hoodarachit07@outlook.com>
2020-12-15 19:22:55 +0530

    basic routes

--- app.py
+++ app.py
@@ -33,3 +33,80 @@ class Product(db.Model):
     date_posted = db.Column(db.DateTime)
     des = db.Column(db.Text)
     repo = db.Column(db.String(50))
+
+@login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    class LoginForm(FlaskForm):
+        username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+        password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+        remember = BooleanField('remember me')
+
+    class RegisterForm(FlaskForm):
+        email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+        username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+        password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('home'))
+
+        return '<h1>Invalid username or password</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1>New user has been created! <a href="/login">Login</a></h1>'
+            #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('signup.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/post/<int:post_id>')
+@login_required
+def post(post_id):
+    """
+    Post page route
+    """
+    pass
+
+
+@app.route('/addpost', methods=['POST', 'GET'])
+@login_required
+def addpost():
+    if request.method == "POST":
+        #request from form
+        pass
+    else:
+        #just render_template
+        pass
