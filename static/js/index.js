0f8f5ff5dbb69e04aeb3c016813737662b8088ac
Zhenyu Liang <65879206+Zhenyu-Liang@users.noreply.github.com>
2020-12-17 11:55:26 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,1 @@
-console.log("This is working")
-window.onscroll = function() {scroll()}
-function scroll(){
-    var s = window.pageYOffset;
-    document.getElementById("a").style.top = ((s/(window.outerHeight+200))*100)+"%";
-}
+
