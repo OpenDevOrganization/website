da5a98e01c0bba9b0cdd3239b5c5fbbbe63d7c83
Rachithooda <hoodarachit07@outlook.com>
2020-12-19 20:07:32 +0530

    login/signup

--- app.py
+++ app.py
@@ -16,11 +16,12 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-# class User(UserMixin, db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(15), unique=True)
-#     email = db.Column(db.String(50), unique=True)
-#     password = db.Column(db.String(80))
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    coins = db.Column(db.Integer)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
 
 # class Product(db.Model):
 #     """
@@ -34,15 +35,15 @@ login_manager.login_view = 'login'
 #     repo = db.Column(db.String(50))
 
 
-#     class LoginForm(FlaskForm):
-#         username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-#         password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-#         remember = BooleanField('remember me')
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
 
-#     class RegisterForm(FlaskForm):
-#         email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-#         username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-#         password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
 
 @login_manager.user_loader
@@ -62,42 +63,42 @@ def home():
 #     return render_template('index.html')
 
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
 
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username=form.username.data).first()
-#         if user:
-#             if check_password_hash(user.password, form.password.data):
-#                 login_user(user, remember=form.remember.data)
-#                 return redirect(url_for('home'))
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('home'))
 
-#         return '<h1>Invalid username or password</h1>'
-#         #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+        return '<h1>Invalid username or password</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
 
-#     return render_template('login.html', form=form)
+    return render_template('login.html', form=form)
 
-# @app.route('/signup', methods=['GET', 'POST'])
-# def signup():
-#     form = RegisterForm()
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
 
-#     if form.validate_on_submit():
-#         hashed_password = generate_password_hash(form.password.data, method='sha256')
-#         new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-#         db.session.add(new_user)
-#         db.session.commit()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, coins=500, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
 
-#         return '<h1>New user has been created! <a href="/login">Login</a></h1>'
-#             #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+        return '<h1>New user has been created! <a href="/login">Login</a></h1>'
+            #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
 
-#     return render_template('signup.html', form=form)
+    return render_template('signup.html', form=form)
 
-# @app.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     return redirect(url_for('index'))
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 # @app.route('/post/<int:post_id>')
@@ -122,4 +123,5 @@ def home():
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
